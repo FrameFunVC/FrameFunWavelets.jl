d2eb0d7c81d3f414d8bd7963c1877897317a9c88
vincentcp <vincent.coppe@gmail.com>
2018-07-18 00:22:14 +0200

    fix type WaveletsCopy clone

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,4 @@
-Pkg.clone("https://github.com/vincentcp/WaveletsCopy.git")
-# Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl.git")
+Pkg.clone("https://github.com/vincentcp/WaveletsCopy.jl.git")
 Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git")
 Pkg.checkout("BasisFunctions","extract")
 Pkg.build("BasisFunctions")
