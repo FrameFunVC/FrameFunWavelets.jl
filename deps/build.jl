fee0270cdf462880f36aba25ee752235241899b3
vincentcp <vincent.coppe@gmail.com>
2018-07-18 00:32:02 +0200

    build WaveletsCopy

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,5 @@
 Pkg.clone("https://github.com/vincentcp/WaveletsCopy.jl.git")
+Pkg.build("WaveletsCopy")
 Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git")
 Pkg.checkout("BasisFunctions","extract")
 Pkg.build("BasisFunctions")
