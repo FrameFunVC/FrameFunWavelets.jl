c95aa49688ee7a030838d40c50305515068ff1bd
vincentcp <vincent.coppe@gmail.com>
2018-07-26 07:43:59 +0200

    change basisfunctions branch

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,6 @@ if VERSION < v"0.7-"
     Pkg.clone("https://github.com/vincentcp/WaveletsCopy.jl.git")
     Pkg.build("WaveletsCopy")
     Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git")
-    Pkg.checkout("BasisFunctions","extract")
+    Pkg.checkout("BasisFunctions","julia-0.7")
     Pkg.build("BasisFunctions")
-    Pkg.clone("https://github.com/daanhb/Domains.jl.git")
 end
